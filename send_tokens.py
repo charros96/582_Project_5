838ad910b47c8a3439ed072040c49acc23304fbe
charros96 <88594194+charros96@users.noreply.github.com>
2022-04-23 22:28:22 -0700

    Add files via upload

--- send_tokens.py
+++ send_tokens.py
@@ -49,7 +49,7 @@ def send_tokens_algo( acl, sender_sk, txes):
     for i,tx in enumerate(txes):
         #tx = transaction.PaymentTxn(pk, tx_fee, first_valid_round, last_valid_round, gen_hash, receiver_pk, tx_amount, flat_fee=True)
         print(tx[i])
-        unsigned_tx = transaction.PaymentTxn(sender_pk, tx_fee, first_valid_round, last_valid_round, gen_hash, tx.receiver_pk, tx.amount, flat_fee=True)
+        unsigned_tx = transaction.PaymentTxn(sender_pk, tx_fee, first_valid_round, last_valid_round, gen_hash, tx.get('receiver_pk'), tx.get('amount'), flat_fee=True)
 
         # TODO: Sign the transaction
         signed_tx = tx.sign(sender_sk)
@@ -136,7 +136,7 @@ def send_tokens_eth(w3,sender_sk,txes):
         tx_dict = {
                 'nonce': starting_nonce+i, #Locally update nonce
                 'gasPrice':w3.eth.gas_price,
-                'gas': w3.eth.estimate_gas( { 'from': sender_pk, 'to': tx.receiver_pk, 'data': b'', 'amount': tx.amount } ),
+                'gas': w3.eth.estimate_gas( { 'from': sender_pk, 'to': tx.get('receiver_pk'), 'data': b'', 'amount': tx.get('amount') } ),
                 'to': tx.receiver_pk,
                 'value': tx.amount,
                 'data':b'' }
