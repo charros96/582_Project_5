b6eb2651fbc716942cf05dee0b8a6a75fa90c542
charros96 <88594194+charros96@users.noreply.github.com>
2022-04-24 12:22:14 -0700

    Add files via upload

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -223,10 +223,10 @@ def fill_order(order, txes=[]):
                         
                             g.session.commit()
                             tx_parent = {'platform':parent.buy_currency,'receiver_pk':parent.receiver_pk,'order_id':parent.id,'amount':int(counter.sell_amount)}
-                            print(tx_parent['platform'],tx_parent['amount'],file=sys.stderr)
+                            #print(tx_parent['platform'],tx_parent['amount'],file=sys.stderr)
                             #print(tx_order['receiver_pk'])
                             tx_counter = {'platform':counter.buy_currency,'receiver_pk':counter.receiver_pk,'order_id':counter.id,'amount': int(counter.buy_amount)}
-                            print(tx_counter['platform'],tx_counter['amount'],file=sys.stderr)
+                            #print(tx_counter['platform'],tx_counter['amount'],file=sys.stderr)
                             algo_id, eth_id = execute_txes([tx_parent,tx_counter])
                             tx_fields = ['platform','receiver_pk','order_id']
                             tx_obj = TX(**{f:tx_parent[f] for f in tx_fields})
@@ -243,10 +243,10 @@ def fill_order(order, txes=[]):
                             g.session.commit()
                             break
                         tx_parent = {'platform':order.buy_currency,'receiver_pk':order.receiver_pk,'order_id':order.id,'amount': int(order.buy_amount)}
-                        print(tx_parent['platform'],tx_parent['amount'],file=sys.stderr)
+                        #print(tx_parent['platform'],tx_parent['amount'],file=sys.stderr)
                         #print(tx_order['receiver_pk'])
                         tx_counter = {'platform':existing_order.buy_currency,'receiver_pk':existing_order.receiver_pk,'order_id':existing_order.id,'amount': int(existing_order.buy_amount)}
-                        print(tx_counter['platform'],tx_counter['amount'],file=sys.stderr)
+                        #print(tx_counter['platform'],tx_counter['amount'],file=sys.stderr)
                         algo_id, eth_id = execute_txes([tx_parent,tx_counter])
                         tx_fields = ['platform','receiver_pk','order_id']
                         tx_obj = TX(**{f:tx_parent[f] for f in tx_fields})
